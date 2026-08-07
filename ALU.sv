d380351d6c6c64384b9a8d5afbabc5d30d5674a5
Dreamy-source <gorja.goiu@gmail.com>
2026-08-07 19:03:38 +0700

    reworked alu

--- ALU.sv
+++ ALU.sv
@@ -13,6 +13,31 @@ module ALU (
         OVERFLOW_FLAG            = 0;
         DIV_BY_ZERO_EXCEPTION    = 0;
         UNKNOWN_OPCODE_EXCEPTION = 0;
-    end
 
+        case (OPCODE)
+            16'h0000: begin end                                                    // 0x0000 - nothing
+            16'h0001: begin {OVERFLOW_FLAG, SUM} = NUM0  +  NUM1;          end     // 0x0001 - add
+            16'h0002: begin {OVERFLOW_FLAG, SUM} = NUM0  -  NUM1;          end     // 0x0002 - sub
+            16'h0003: begin {OVERFLOW_FLAG, SUM} = NUM0  *  NUM1;          end     // 0x0003 - mul
+            16'h0004: begin                                                        // 0x0004 - div
+                if (NUM1 != 64'b0) begin
+                    {OVERFLOW_FLAG, SUM} = NUM0 / NUM1;
+                end else begin
+                    SUM                   = 64'b0;
+                    DIV_BY_ZERO_EXCEPTION = 1;
+                end
+            end
+            16'h0005: begin {OVERFLOW_FLAG, SUM} = NUM0  &  NUM1;          end     // 0x0005 - and
+            16'h0006: begin {OVERFLOW_FLAG, SUM} = NUM0  |  NUM1;          end     // 0x0006 - or
+            16'h0007: begin {OVERFLOW_FLAG, SUM} = NUM0  ^  NUM1;          end     // 0x0007 - xor
+            16'h0008: begin {OVERFLOW_FLAG, SUM} =~NUM0;                   end     // 0x0008 - not
+            16'h0009: begin {OVERFLOW_FLAG, SUM} = NUM0  << NUM1;          end     // 0x0009 - lshft
+            16'h000A: begin {OVERFLOW_FLAG, SUM} = NUM0  >> NUM1;          end     // 0x000A - rshft
+            16'h000B: begin {OVERFLOW_FLAG, SUM} = $signed(NUM0) >>> NUM1; end     // 0x000B - asr
+            default: begin
+                SUM                      = 64'b0;
+                UNKNOWN_OPCODE_EXCEPTION = 1;
+            end
+        endcase
+    end
 endmodule
