4e9832d6c7e8e82defc933606fb06cb022895474
Dreamy-source <gorja.goiu@gmail.com>
2026-08-07 21:33:40 +0700

    reworked asm #2

--- ASM/files/input.asm
+++ ASM/files/input.asm
@@ -1,5 +1,6 @@
-reg rx2 = 5
 reg rx1 = 3
+reg rx2 = 0x05
+reg rx3 = 0001b
 
 add rx14 = rx0  | rx1
 sub rx15 = rx2  | rx3
@@ -8,6 +9,9 @@ div rx17 = rx6  | rx7
 and rx18 = rx8  | rx9
 or  rx19 = rx10 | rx11
 xor rx20 = rx12 | rx13
+
+meow meow meow meow, meow!! abcdef, meowmeoww meow
+idk
 
 nothing
 ; meow meow
--- ASM/vn.rs
+++ ASM/vn.rs
@@ -12,7 +12,7 @@ use pl::instructions_to_txt;
 
 pub fn register_to_hex(reg: &str) -> u8 {
     let reg_parsed: u8 = reg.trim_start_matches("rx").parse()
-        .expect(&format!(" in machine: unknown register: '{}'", reg));
+        .expect(&format!("    [?] machine: unknown register: '{}'", reg));
 
     reg_parsed
 }
@@ -32,13 +32,23 @@ pub fn opcode_to_hex(op: &str) -> u16 {
         "rshft"   => 0x000A,
         "asr"     => 0x000B,
         "reg"     => 0x000C,
-        _ => panic!(" in machine: unknown opcode: '{}'", op),
+        _ => panic!("    [?] machine: unknown opcode: '{}'", op),
     }
 }
 
 pub fn value_to_hex(val: &str) -> u64 {
-    let val_parsed: u64 = u64::from_str_radix(val, 16)
-        .expect(&format!(" in machine: unknown value: '{}'", val));
+    let (radix, val) = if val.starts_with("0x") {
+        (16, &val[2..])
+    } else if val.starts_with("0b") {
+        (2, &val[2..])
+    } else if val.ends_with('b') {
+        (2, &val[..val.len()-1])
+    } else {
+        (10, val)
+    };
+
+    let val_parsed: u64 = u64::from_str_radix(val, radix)
+        .expect(&format!("    [?] machine: unknown value: '{}'", val));
 
     val_parsed
 }
@@ -75,6 +85,7 @@ fn main() {
 
         match opcode {
             "reg" => {
+                println!("  \n[prc] decoding: '{}'", opcode);
                 let op  = opcode_to_hex(parts[0]);
                 let dst = register_to_hex(parts[1]);
                 let val = value_to_hex(parts[3]);
@@ -84,10 +95,12 @@ fn main() {
                     (dst as u64) << 32 |
                     (val as u64);
                 
+
                 instructions.push(instruction);
-                println!(" [ok] in machine: decoded: 0x{:064X}", instruction);
+                println!(" [ok] machine: decoded: 0x{:064X}", instruction);
             }
             "add" | "sub" | "mul" | "div" | "and" | "or" | "xor" | "lshft" | "rshft" | "asr" => {
+                println!("  \n[prc] decoding: '{}'", opcode);
                 let op   = opcode_to_hex(parts[0]);
                 let dst  = register_to_hex(parts[1]);
                 let reg0 = register_to_hex(parts[3]);
@@ -98,18 +111,24 @@ fn main() {
                     (dst  as u64) << 43 |
                     (reg0 as u64) << 38 |
                     (reg1 as u64) << 33;
-                    
+                
                 instructions.push(instruction);
-                println!(" [ok] in machine: decoded: 0x{:064X}", instruction);
+                println!(" [ok] machine: decoded: 0x{:064X}", instruction);
             }
             "nothing" => {
                 let op = opcode_to_hex(parts[0]);
 
                 let instruction: u64 = op as u64;
                 instructions.push(instruction);
-                println!(" [ok] in machine: decoded: 0x{:064X}", instruction);
+                println!(" [ok] machine: decoded: 0x{:064X}", instruction);
             }
-            _ => {}
+            _ => {
+                if opcode == "meow" {
+                    println!("    [] meow :3");
+                } else {
+                    println!("    [?] machine: cannot decode: '{}', skipping.", opcode);
+                }
+            }
         }
     }
 
