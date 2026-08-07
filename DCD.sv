3a21f8e2ed6a0a6252e163bb0347a9e64b2ebc89
Dreamy-source <gorja.goiu@gmail.com>
2026-08-07 19:10:48 +0700

    reworked dcd

--- DCD.sv
+++ DCD.sv
@@ -1,26 +1,16 @@
-`include "MIC.sv"
-
 module DCD (
-    input  logic        RS_TYPE,
     input  logic [63:0] INSTRUCTION,
     output logic [15:0] OPCODE,
     output logic [6:0]  REG_DESTINATION,
-    output logic [6:0]  REG_0,
-    output logic [6:0]  REG_1,
-    output logic [40:0] IMMEDIATE
+    output logic [6:0]  REG0,
+    output logic [6:0]  REG1,
+    output logic [26:0] IMMEDIATE
 );
     always_comb @(*) begin
         OPCODE          = INSTRUCTION[63:48];
         REG_DESTINATION = INSTRUCTION[47:41]; 
-
-        if (RS_TYPE == 1'b0) begin                   // 0 = S
-            REG_0           = 7'b0;
-            REG_1           = 7'b0;
-            IMMEDIATE       = INSTRUCTION[40:0];
-        end else begin                               // 1 = R
-            REG_0           = INSTRUCTION[40:34];
-            REG_1           = INSTRUCTION[33:27];
-            IMMEDIATE       = 41'b0;
-        end
+        REG0            = INSTRUCTION[40:34];
+        REG1            = INSTRUCTION[33:27];
+        IMMEDIATE       = INSTRUCTION[26:0];
     end
 endmodule
