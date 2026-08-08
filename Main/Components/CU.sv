9b21623469a646cf2b197e005c2f3eedf20bda5d
Dreamy-source <gorja.goiu@gmail.com>
2026-08-08 12:16:27 +0700

    CU +ALU

--- Main/Components/CU.sv
+++ Main/Components/CU.sv
@@ -5,7 +5,22 @@
 `include "RF.sv"
 
 module CU;
+    logic [15:0] alu_opcode;
+    logic [63:0] alu_num0;
+    logic [63:0] alu_num1;
+    logic [63:0] alu_sum;
+    logic        alu_overflow_flag;
+    logic        alu_div_by_zero_exception;
+    logic        alu_unknown_opcode_exception;
 
-    // ...
+    ALU alu (
+        .OPCODE(alu_opcode),
+        .NUM0(alu_num0),
+        .NUM1(alu_num1),
+        .SUM(alu_sum),
+        .OVERFLOW_FLAG(alu_overflow_flag),
+        .DIV_BY_ZERO_EXCEPTION(alu_div_by_zero_exception),
+        .UNKNOWN_OPCODE_EXCEPTION(alu_unknown_opcode_exception)
+    );
 
 endmodule
