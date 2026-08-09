4bcffd7b84db017b7f038b798d59c318e1182b2b
Dreamy-source <gorja.goiu@gmail.com>
2026-08-09 11:16:26 +0700

    ROM finished (mb partial)

--- Main/Components/ROM.sv
+++ Main/Components/ROM.sv
@@ -1,6 +1,15 @@
 // ROM (Read-Only Memory)
-module ROM;
+module ROM (
+    input  logic [63:0] ADDR,
+    output logic [63:0] INSTRUCTION
+);
+     localparam rom_maxsize      = 65536;
+     localparam rom_maxsize_bits = 16;
+     logic [63:0] rom [0:rom_maxsize-1];
 
-    // ...
-    
+     initial begin
+        $readmemh("rom.hex", rom);
+     end
+
+     assign INSTRUCTION = rom[ADDR[rom_maxsize_bits-1:0]];
 endmodule
