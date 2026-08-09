0e3138f03e8cca53fc6cbbd2c84955e8003aff96
Dreamy-source <gorja.goiu@gmail.com>
2026-08-09 11:25:57 +0700

    PC finished (mb partial)

--- Main/Components/PC.sv
+++ Main/Components/PC.sv
@@ -1,7 +1,18 @@
 // PC (Program Counter)
-
-module PC;
-
-    // ...
-
+module PC (
+    input  logic        CLK,
+    input  logic        RST,
+    input  logic [63:0] NEXT_COUNT,
+    input  logic        NEXT_COUNT_FLAG,
+    output logic [63:0] COUNT
+);
+    always_ff @(posedge CLK) begin
+        if (RST) begin
+            COUNT <= 64'b0;
+        end else if (NEXT_COUNT_FLAG) begin
+            COUNT <= NEXT_COUNT;
+        end else begin
+            COUNT <= COUNT + 64'b1;
+        end
+    end
 endmodule
