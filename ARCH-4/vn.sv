1dec6f4cba2676da839c4f63f6b66b13eee2b2ab
Dreamy-source <gorja.goiu@gmail.com>
2026-08-14 23:37:32 +0700

    +++

--- ARCH-4/vn.sv
+++ ARCH-4/vn.sv
@@ -11,6 +11,7 @@ interface ALU_if;
     logic [8:0]  operation;
     logic [63:0] num0,num1;
     logic [63:0] sum;
+    logic        ov,dz,uo;
 
     modport alu (
         input  operation, num0, num1
@@ -20,7 +21,24 @@ endinterface
 
 
 module ALU (ALU_if.alu intf);
+    always_comb begin
+        sum      = 64'b0;
+        ov,dz,uo = 1'b0
 
+        case (intf.operation)
+            9'h001: {intf.ov,intf.sum} = intf.num0 + intf.num1;
+            9'h002: {intf.ov,intf.sum} = intf.num0 - intf.num1;
+            9'h003: {intf.ov,intf.sum} = intf.num0 * intf.num1;
+            9'h004: begin
+                if (intf.num1 != 64'b0) begin
+                    {intf.ov,intf.sum} = intf.num0 / intf.num1;
+                end else begin
+                    intf.sum = 64'b0;
+                    intf.dz  = 1'b1;
+                end
+            end
+        endcase
+    end
 endmodule
 
 module RF (RF_if.rf intf);
