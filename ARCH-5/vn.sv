71141146ff0f1e168cefb4629d87f9f2fd82d1ff
Dreamy-source <gorja.goiu@gmail.com>
2026-08-22 16:10:21 +0700

    Added testbench, operations in ALU module (sub, mul)

--- ARCH-5/vn.sv
+++ ARCH-5/vn.sv
@@ -1,14 +1,22 @@
 module alu (
+    input  logic [4:0]  Operation,
     input  logic [63:0] A, B,
     output logic [63:0] Result
 );
-    assign Result = A + B;
+    always_comb begin
+        case (Operation)
+            5'h00: Result = A + B;
+            5'h01: Result = A - B;
+            5'h02: Result = A * B;
+            default: Result = '0;
+        endcase
+    end
 endmodule
 
 module regfile (
     input  logic        clock, reset, write,
-    input  logic [3:0]  register,
-    input  logic [63:0] data
+    input  logic [3:0]  Register,
+    input  logic [63:0] Data
 );
     logic [63:0] rx [0:7];
 
@@ -18,7 +26,7 @@ module regfile (
                 rx[i] <= 'b0;
             end
         end else if (write) begin
-            rx[register] <= data;
+            rx[Register] <= Data;
         end
     end
 endmodule
@@ -26,27 +34,30 @@ endmodule
 module controlunit (
     input  logic clock, reset, write
 );
+    logic [4:0]  ALU_Operation;
     logic [63:0] ALU_A, ALU_B;
     logic [63:0] ALU_Result;
     
-    logic [63:0] RF_Data;
     logic [3:0]  RF_Register;
+    logic [63:0] RF_Data;
     
     alu ALU (
+        .Operation(ALU_Operation),
         .A(ALU_A),
         .B(ALU_B),
         .Result(ALU_Result)
     );
-    
+
     regfile RF (
         .clock(clock),
         .reset(reset),
         .write(write),
-        .register(RF_Register),
-        .data(RF_Data)
+        .Register(RF_Register),
+        .Data(RF_Data)
     );
 
-    // получатель = отправитель
+    // recipient = sender
+    assign ALU_Operation = 5'h00;
     assign ALU_A = 'd5;
     assign ALU_B = 'd3;
     assign RF_Data = ALU_Result;
@@ -54,8 +65,12 @@ module controlunit (
 
     // testbench
     initial begin
+        #10;
+        $display("---- vanilla general ----");
+        $display("Operation = 0x%h", ALU_Operation);
         $display("A = %0d (0x%0h)", ALU_A, ALU_A);
         $display("B = %0d (0x%0h)", ALU_B, ALU_B);
         $display("rx0 = %0d (0x%0h)", RF_Data, RF_Data);
+        $finish;
     end
-endmodul
+endmodule
